206c59f7ff2e5196312b23c6285b977441c19efd
dnercred <dnercred@gmail.com>
2022-12-18 21:38:46 -0300

    Estudos De java Script

---
+++ asd.js
@@ -0,0 +1,1 @@
+console.log ("denner santos brenner santos ");
---
+++ desafio.js
@@ -0,0 +1,22 @@
+/* Desafio de  senteças de codigos com Blocos de Codigos
+  Exigir na tela seguencias de numeros pares e impas 
+
+
+  Obs imprimir dos BLocos separamente 
+*/
+
+{
+    console.log(('Desafio Blocos De numeros impares') ,1 , 3 ,5 ,7 ,9)
+
+
+};
+
+
+{
+    console.log(('Desafio Blocos De numeros Pares'),2 , 4 ,6 ,8 ,10)
+
+
+};
+
+console.log('Otimo (a) Voce Consegui execultar o primeiro Desafio' );
+
