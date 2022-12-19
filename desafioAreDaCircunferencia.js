534733b17a9cee297536d316bbcc43fdba78cc5f
dnercred <dnercred@gmail.com>
2022-12-18 22:36:19 -0300

    commit 6 Desafio Area da circuferencia

---
+++ desafioAreDaCircunferencia.js
@@ -0,0 +1,26 @@
+/* Calcular a area de uma circuferencia */
+
+/*Valor do PI= 3,14 vezes o valor do Raio ao Quadrado*/
+
+const PI = 3.14
+let raio = 10
+
+
+
+let ValorDaCircuferencia = (PI*raio*raio);
+
+let resultado = ValorDaCircuferencia;
+
+let m2 = "metros QUadrados"
+
+console.log("Valor da circuferencia: é igual " + resultado +  m2);
+
+
+
+
+/*Exemplo do CUrso */
+
+const PI = 3.14
+let raio = 10
+let areacirc =PI *raio*raio;
+console.log('o valor do desafio '+ areacirc);
