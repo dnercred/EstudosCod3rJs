a21acf11bd0c39ef1004e93477fcfb7918dd858d
dnercred <dnercred@gmail.com>
2022-12-18 22:23:20 -0300

    commit 5

---
+++ trabalhandoComDados4.js
@@ -0,0 +1,37 @@
+
+
+/*Const é uma Variavel constante ouse seja uma variavel
+que nao Muda o seu Valor */
+
+
+
+/*exemplo alterar o valor de uima variavel*/
+
+
+
+const = a
+
+/* aqui no caso o codigo vai da errro pois nao pode modificar os dados da vairiavel 
+quando ela é cont 
+*/
+
+let a  =19.90;
+a = a+2650;
+
+
+
+
+
+console.log(a);
+
+
+
+
+/*Const é uma Variavel constante ouse seja uma variavel
+que nao Muda o seu Valor */
+
+
+
+/*Obs E fundamental de importante Definir BOns nomes nas Variavaies funcoes , Entre outras 
+observaçoes importantes*/
+
