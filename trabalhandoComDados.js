87ad83ebb20859ad1934ac03235e76c8712e13c3
dnercred <dnercred@gmail.com>
2022-12-18 21:46:54 -0300

    commit 2

---
+++ trabalhandoComDados.js
@@ -0,0 +1,14 @@
+var nome = 'DENNER';
+
+var sobrenome = 'SAntos';
+var idade = '30 anos';
+var Profissao = 'Programador';
+var roben = 'Tocar Guitarra ';
+
+
+
+console.log(nome);
+console.log(idade);
+console.log(sobrenome);  //);
+console.log(nome);
+console.log(nome);
