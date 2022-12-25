7445c77180ab384323280eee551012a25ba04f65
dnercred <dnercred@gmail.com>
2022-12-24 23:02:21 -0300

    subindo arquivos

---
+++ desafioTrocaDeDados.js
@@ -0,0 +1,19 @@
+/* DESAFIO de troca de Dados no js , TROCAR O VALOR DA VARIAVEL */
+
+
+
+
+let a = 7 ;
+
+let b = 9 ;
+
+let temp = a;
+
+a = b ;
+b = temp;
+
+console.log(a);
+console.log(b);
+
+
+/* Comando temp modifica a variavel alocando o valor especificado na variavel que eu quero que apareça*/
